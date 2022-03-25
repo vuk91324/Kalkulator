8d3878a7d9878800911d94165d3559a6e66e1389
Vuk Radmilovic <2001vuk@gmail.com>
2022-03-25 18:44:00 +0100

    Add the summation functionality

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@
                 } else {
                     double n1 = double.Parse(arguments[0]);
                     double n2 = double.Parse(arguments[2]);
-                    
+                    double result = n1 + n2;
+                    Console.WriteLine(n1 + " + " + n2 + " = " + result);
                 }
                 input = Console.ReadLine();
             }

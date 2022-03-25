c337439a8d32d153d13ff1cf69c6769a4e0ec103
Vuk Radmilovic <2001vuk@gmail.com>
2022-03-25 18:26:20 +0100

    Added user input functionality

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
         }
     }
 }

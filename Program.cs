cca9580aa5d337d382bb4ed19895d98130c95eb4
Vuk Radmilovic <2001vuk@gmail.com>
2022-03-25 18:52:24 +0100

    Add substraction

--- Program.cs
+++ Program.cs
@@ -8,12 +8,22 @@
             while (input != "exit" && input != "kraj") {
                 string[] arguments = input.Split();
                 if (arguments.Length != 3) {
-                    Console.WriteLine("Neispravan ulaz!");
+                    Console.WriteLine("Neispravan broj argumenata!");
                 } else {
                     double n1 = double.Parse(arguments[0]);
                     double n2 = double.Parse(arguments[2]);
-                    double result = n1 + n2;
-                    Console.WriteLine(n1 + " + " + n2 + " = " + result);
+                    double result = -1;
+                    Boolean err = false;
+                    if (arguments[1] == "+") {
+                        result = n1 + n2;
+                    } else if (arguments[1] == "-") {
+                        result = n1 - n2;
+                    } else {
+                        Console.WriteLine("Neispravan operator!");
+                        err = true;
+                    }
+                    if (!err)
+                        Console.WriteLine(n1 + " " + arguments[1] +" " + n2 + " = " + result);
                 }
                 input = Console.ReadLine();
             }
